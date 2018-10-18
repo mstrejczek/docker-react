e0364df899c2adb3324e3d7f16f74e98f570c3a9
Marek Strejczek <marek.strejczek@tomtom.com>
2018-10-18 21:59:24 +0200

    Added docker-compose

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hi there!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <h1>Ok bye bye now - time to sleep</h1>
           <a
             className="App-link"
             href="https://reactjs.org"
